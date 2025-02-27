e9fc490b19e99a160bc7b27b4511d71c62039547
Tanjim-king <apontanjim2@gmail.com>
2025-02-27 22:02:53 +0600

    Remove picture url input and add picture upload sys.

--- public/js/addblog.js
+++ public/js/addblog.js
@@ -1,40 +1,57 @@
-const {createApp} = Vue ;
+const { createApp } = Vue;
 
 const app = createApp({
-    data(){
-        return{
-            thumb:"",
-            title : "",
-            desc:"",
-            name : "",
-            type : ""
-        }
+    data() {
+        return {
+            thumb: "",  // This will store the Base64 image
+            title: "",
+            desc: "",
+            name: "",
+            type: ""
+        };
     },
-    methods:{
-        addBlog(){
-            let d = new Date()
-            let info ={
-                author : this.name,
-                title : this.title,
-                desc : this.desc,
-                img : this.thumb,
-                date : d.toLocaleDateString('bn-BD',{
-                    day : 'numeric',
-                    month:'long',
-                    year : 'numeric'
-                }),
-                type:this.type
+    methods: {
+        // Convert uploaded image to Base64
+        convertToBase64(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => {
+                    this.thumb = reader.result; // Store Base64 data in thumb
+                };
             }
+        },
+        addBlog() {
+            let d = new Date();
+            let info = {
+                author: this.name,
+                title: this.title,
+                desc: this.desc,
+                img: this.thumb, // Send Base64 image
+                date: d.toLocaleDateString('bn-BD', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                }),
+                type: this.type
+            };
+
             if (this.thumb && this.title && this.desc && this.name && this.type) {
-                axios.post("http://localhost:5050/blogs" , info)
-                    .then(res =>{
-                        alert("Your blog is succesfull add")
-                        window.open("/" ,"_self")
+                axios.post("http://localhost:5050/blogs", info)
+                    .then(res => {
+                        alert("Your blog has been successfully added!");
+                        window.open("/", "_self");
                     })
+                    .catch(err => {
+                        alert("Error adding blog. Please try again.");
+                        console.error(err);
+                    });
             } else {
-                alert("Inputs can not be empty")
+                alert("Inputs cannot be empty");
             }
         }
     }
-})
-app.mount("#app")
+});
+
+app.mount("#app");
